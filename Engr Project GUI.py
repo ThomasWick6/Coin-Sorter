fe339b1ed4fd420c88ec6831b9a7c21c3adf4737
ThomasWick6 <101420845+ThomasWick6@users.noreply.github.com>
2022-05-05 10:20:43 -0500

    Update Engr Project GUI.py

--- Engr Project GUI.py
+++ Engr Project GUI.py
@@ -13,14 +13,16 @@ nval = 5.0
 qval = 5.67
 
 
-WIDTH = 1200  #width/hight          #sets width, hight, and name for person who it's for
-HEIGHT = 800
+WIDTH = 800  #width/hight          #sets width, hight, and name for person who it's for
+HEIGHT = 600
 name = "Tommy"
 
 arduino = serial.Serial(port='COM3', baudrate=57600, timeout=.1)
-
 time.sleep(10)
+
 def ar_read():
+    #return 5,5,5,5
+    
     while True:
         val = 'p'
         arduino.write(bytes(val,'utf-8'))
@@ -39,8 +41,8 @@ def ar_read():
             
                 listdata.append(fdata)
 
-#                if (len(listdata)==4):
- #                   break
+                if (len(listdata)==4):
+                    break
             
             except:
                 pass
@@ -73,29 +75,29 @@ class main(Frame):
     def GUI(self):
 
         
-        welcome = Label(self.master, text = "Welcome, {}!".format(name), font = ("Arial", 20),).place(x = 470, y = 50)
+        welcome = Label(self.master, text = "Welcome, {}!".format(name), font = ("Arial", 20),).place(x = 310, y = 50)
     
         dimes,pennys,nickles,quarters = ar_read()
              
         balance = pennys*0.01 + nickles*0.05 + dimes*0.1 + quarters*0.25
         balance = round(balance,2)
 
-        totallabel = Label(self.master, text = "Your total balance", font = ("Arial", 18)).place(x = 490, y = 80)
-        total = Label(self.master, text = "${}".format(balance), font = ("Arial", 18), fg = "red").place(x = 500, y = 120)      #The 
+        totallabel = Label(self.master, text = "Your total balance", font = ("Arial", 18)).place(x = 320, y = 80)
+        total = Label(self.master, text = "${}".format(balance), font = ("Arial", 18), fg = "red").place(x = 380, y = 120)      #The 
 
-        pennylabel = Label(self.master, text = "Number of pennies", font = ("Arial", 18)).place(x = 100, y = 200)
-        penny = Label(self.master, text = "{}".format(pennys), font = ("Arial", 18), fg = "red").place(x = 180, y = 260)
+        pennylabel = Label(self.master, text = "Pennies", font = ("Arial", 18)).place(x = 100, y = 200)
+        penny = Label(self.master, text = "{}".format(pennys), font = ("Arial", 18), fg = "red").place(x = 160, y = 260)
         
         
-        nicklelabel = Label(self.master, text = "Number of nickles", font = ("Arial", 18)).place(x = 350, y = 200)
-        nickle = Label(self.master, text = "{}".format(nickles), font = ("Arial", 18), fg = "red").place(x = 380, y = 260)
+        nicklelabel = Label(self.master, text = "Nickles", font = ("Arial", 18)).place(x = 250, y = 200)
+        nickle = Label(self.master, text = "{}".format(nickles), font = ("Arial", 18), fg = "red").place(x = 310, y = 260)
         
         
-        dimelabel = Label(self.master, text = "Number of dimes", font = ("Arial", 18)).place(x = 600, y = 200)
-        dime = Label(self.master, text = "{}".format(dimes), font = ("Arial", 18), fg = "red").place(x = 680, y = 260)
+        dimelabel = Label(self.master, text = "Dimes", font = ("Arial", 18)).place(x = 400, y = 200)
+        dime = Label(self.master, text = "{}".format(dimes), font = ("Arial", 18), fg = "red").place(x = 460, y = 260)
         
-        quarterlabel = Label(self.master, text = "Number of quarters", font = ("Arial", 18)).place(x = 850, y = 200)
-        quarter = Label(self.master, text = "{}".format(quarters), font = ("Arial", 18), fg = "red").place(x = 880, y = 260)        
+        quarterlabel = Label(self.master, text = "Quarters", font = ("Arial", 18)).place(x = 550, y = 200)
+        quarter = Label(self.master, text = "{}".format(quarters), font = ("Arial", 18), fg = "red").place(x = 610, y = 260)        
 
         
 def runbank():
@@ -111,6 +113,8 @@ U = main(window)
 U.GUI()
 runbank()
 window.mainloop()
+
+
 
 
 
